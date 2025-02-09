3948e54d7b7c61086de48890def95d9ebe0d227e
ramveermeena2003 <ramveermeena2003@gmail.com>
2025-02-09 19:40:54 +0530

    initial commit

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js"
+import { connectDB } from "./lib/db.js";
+
+
+const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes)
+const PORT = process.env.PORT;
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+    connectDB();
+})
