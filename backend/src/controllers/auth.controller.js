14bf4885baa1f11cce688b6969311885ff9712a5
ramveermeena2003 <ramveermeena2003@gmail.com>
2025-02-10 15:20:14 +0530

    update auth.controller file (login and logout function)

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,6 +2,8 @@ import { generateTokens } from "../lib/utlis.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs"
 
+
+// signup 
 export const signup = async (req,res)=>{
     const {fullName,email,password} = req.body;
     try{
@@ -51,16 +53,47 @@ export const signup = async (req,res)=>{
         res.status(500).json({message : "Internal Server Error"})
     }
 }
-export  const login = (req,res)=>{
+
+// login
+export  const login = async (req,res)=>{
     const {email, password} = req.body;
     try{
+        const user = await User.findOne({email});
 
+        if(!user)
+        {
+            return res.status(400).json({message : "Invalid Credentials"});
+        }
+        
+        const isPasswordCorrect = await bcrypt.compare(password,user.password)
+        
+        if(!isPasswordCorrect)
+        {
+                return res.status(400).json({message : "Invalid Credentials"});        
+        }
+
+        generateTokens(user._id,res);
+
+        res.status(200).json({
+            _id : user._id,
+            fullName : user.fullName,
+            email : user.email,
+            profilePic:user.profilePic
+        })
     }
     catch(error){
-
+        console.log("Error in login controller",error.message);
+        res.status(500).json({message: "Internal Server Error"});
     }
 }
 
+// logout
 export const logout = (req,res)=>{
-    res.send("logout route");
+    try {
+        res.cookie("jwt","",{maxAge:0});
+        res.status(200).json({message:"Logged out successfully"});
+    } catch (error) {
+        console.log("Error in logout controller",error.message);
+        res.status(500).json({message:"Internal Server Error"});
+    }
 }
